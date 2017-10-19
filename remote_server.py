99b9b9345bccdcd6d5c19b1fa11837e05faa4f42
Kevin Balke <fughilli@gmail.com>
2017-10-19 07:37:44 +0000

    bug fixes

--- remote_server.py
+++ remote_server.py
@@ -12,11 +12,11 @@ import hexapod
 
 num_directions = 24
 droutines = [
-    lib.motion.read_routine('leg2_walk_d%d.dat' % (direction,))
+    lib.motion.read_routine('path/leg2_walk_d%d.dat' % (direction,))
     for direction in range(num_directions)
 ]
-raise_routine = lib.motion.read_routine('leg2_raise.dat')
-lower_routine = lib.motion.read_routine('leg2_lower.dat')
+raise_routine = lib.motion.read_routine('path/leg2_raise.dat')
+lower_routine = lib.motion.read_routine('path/leg2_lower.dat')
 
 THRESHOLD_FILL_LEVEL = 3
 
@@ -36,7 +36,7 @@ class MotionPlanner(object):
 
     def __init__(self, mcs, dominants, nondominants, doffsets, droutines,
                  raise_routine, lower_routine):
-        self.state = STATE_IDLE
+        self.state = self.STATE_IDLE
         self.waitset = mcs
 
         self.droutines = droutines
@@ -59,22 +59,25 @@ class MotionPlanner(object):
         self.walk = False
 
     def need_update(self):
+        if self.waitset == []:
+            return True
+
         for mc in self.waitset:
-            if mc.queue_depth() < THRESHOLD_FILL_LEVEL:
+            if mc.depth() < THRESHOLD_FILL_LEVEL:
                 return True
         return False
 
     def get_next_plan(self):
-        if self.state == STATE_IDLE:
-            if walk == False:
-                pass
+        if self.state == self.STATE_IDLE:
+            if self.walk == False:
+                self.waitset = []
             else:
-                self.state = STATE_WALKING
+                self.state = self.STATE_WALKING
                 self.waitset = self.nondominant_mcs
                 for mc in self.nondominant_mcs:
                     mc.nqr(self.raise_routine)
-        elif self.state == STATE_WALKING:
-            if walk == True:
+        elif self.state == self.STATE_WALKING:
+            if self.walk == True:
                 self.waitset = self.mcs
                 for mc, doffset in zip(self.dominant_mcs,
                                        self.dominant_doffsets):
@@ -85,42 +88,70 @@ class MotionPlanner(object):
                     mc.nqr(self.droutines_flipped[(doffset + self.direction) %
                                                   len(self.droutines)])
             else:
-                self.state = STATE_IDLE
+                self.state = self.STATE_IDLE
                 self.waitset = self.nondominant_mcs
                 for mc in self.nondominant_mcs:
                     mc.nqr(self.lower_routine)
 
-    def update_task(t, dt):
-        if not self.need_update():
-            return
+    def get_update_task(self):
+        def _update_task(t, dt):
+            if not self.need_update():
+                return
 
-        self.get_next_plan()
+            self.get_next_plan()
+        return _update_task
 
 
 mp = MotionPlanner(mcs, [0, 2, 4], [1, 3, 5], [0, -4, -8, -12, -16, -20],
                    droutines, raise_routine, lower_routine)
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind((socket.gethostname(), 1337))
+serversocket.bind(('', 1337))
 serversocket.listen(1)
+
+run_flag = True
 
 
 def server_thread():
-    while (1):
+    while (run_flag):
         clientsocket, address = serversocket.accept()
         try:
-            cs = util.control.ControlSocket(clientsocket)
-            while (1):
+            cs = lib.control.ControlSocket(clientsocket)
+            while (run_flag):
                 c = cs.receiveControl()
                 mp.direction = c['direction']
                 mp.walk = bool(c['walk'])
         except Exception as e:
             print e.message
             continue
+        clientsocket.close()
 
 
-threading.Thread(target=server_thread).start()
 
-lib.util.looper(
-    lib.util.round_robin_dispatcher((control_update_task, mp.update_task,
-                                     hexapod.motion_plan_task)))
+def motion_plan_task(t, dt):
+    for mc in mcs:
+        mc.update(dt * 2)
+
+
+st = threading.Thread(target=server_thread)
+
+st.start()
+
+for leg in hexapod.legs:
+    leg.enable = True
+
+try:
+    lib.util.looper(
+        lib.util.round_robin_dispatcher(mp.get_update_task(),
+                                        motion_plan_task))
+except Exception as e:
+    pass
+
+for leg in hexapod.legs:
+    leg.enable = False
+
+run_flag = False
+
+st.join()
+
+serversocket.close()
